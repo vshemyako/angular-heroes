f8ccf7b5c62fcd631498001a0110f8599b89e6b3
Valentine Shemyako <v.shemyako@gmail.com>
2019-04-15 22:00:33 +0300

    Created mock array of hero objects

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import {Hero} from './hero';
+
+export const HEROES: Array<Hero> = [
+  {id: 1, name: 'Harry Potter'},
+  {id: 2, name: 'Ron Weasley'},
+  {id: 3, name: 'Draco Malfoy'},
+  {id: 4, name: 'Dobby the House Elf'},
+  {id: 5, name: 'Professor Severus Snape'},
+  {id: 6, name: 'Professor Albus Dumbledore'},
+  {id: 7, name: 'Lord Voldemort'},
+  {id: 8, name: 'Hermione Granger'}
+];
