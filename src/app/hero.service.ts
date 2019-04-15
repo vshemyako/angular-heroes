f1f428566c67ec45407883d2fe995703add058fa
Valentine Shemyako <v.shemyako@gmail.com>
2019-04-15 23:31:29 +0300

    Created MessageService class to provide application messages info

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,12 +2,21 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  public constructor(private messageService: MessageService) {
+  }
+
   public getHeroes(): Observable<Array<Hero>> {
+    this.sendFetchedHeroesMessage();
     return of(HEROES);
+  }
+
+  private sendFetchedHeroesMessage(): void {
+    this.messageService.add('HeroService: fetched heroes');
   }
 }
