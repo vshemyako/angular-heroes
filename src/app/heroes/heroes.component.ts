92de1c11b2a08d9c82d43a781bdf5b9384a761cb
Valentine Shemyako <v.shemyako@gmail.com>
2019-04-15 23:04:03 +0300

    Injected HeroService class, utilized it during init lifecycle hook

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,28 @@
-import {Component} from '@angular/core';
-import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  public heroes: Array<Hero> = HEROES;
+export class HeroesComponent implements OnInit {
+  public heroes: Array<Hero>;
   public hero: Hero;
+
+  constructor(private heroService: HeroService) {
+  }
+
+  ngOnInit(): void {
+    this.refreshHeroes();
+  }
 
   public onClick(hero: Hero): void {
     this.hero = hero;
+  }
+
+  private refreshHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
